5a84855a6c97fa7f17912b62c215980bc6088aae
Don B. Stringham <me@donbstringham.us>
2017-08-27 11:49:37 -0600

    Removed answers

--- src/02-Looping.php
+++ src/02-Looping.php
@@ -1,18 +1,9 @@
 <?php
 
 function DoWhileLoop() {
-    $result = "";
-    $i = 0;
-    do {
-        $result .= "Value of i is " . $i++ . PHP_EOL;
-    } while($i <= 10);
-    return $result;
+    // Your code goes here
 }
 
 function ForLoop() {
-    $result = "";
-    for ($i = 0; $i <= 10; $i++) {
-        $result .= "Value of i is " . $i . PHP_EOL;
-    }
-    return $result;
+    // Your code goes here
 }
