26f846ccee9e42e90f0d4c6f727adb29c7e06e21
Don B. Stringham <me@donbstringham.us>
2017-08-27 09:34:20 -0600

    Initial commit

---
+++ src/00-HelloWorld.php
@@ -0,0 +1,5 @@
+<?php
+
+function GetString() {
+    // Your code goes here
+}
---
+++ src/01-Conditionals.php
@@ -0,0 +1,17 @@
+<?php
+
+function Tgif($day) {
+    // Your code goes here
+}
+
+function DailyMessage($day) {
+    // Your code goes here
+}
+
+function DailyMessageTernary($day) {
+    // Your code goes here
+}
+
+function PageChoice($page) {
+    // Your code goes here
+}
